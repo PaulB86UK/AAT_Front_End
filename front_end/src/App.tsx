33f5a8e39e9ab7a16ce862268e4e3cebca118c84
CrypptoPaul <84225765+CrypptoPaul@users.noreply.github.com>
2022-03-07 20:33:49 +0000

    last changes

--- front_end/src/App.tsx
+++ front_end/src/App.tsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import { DAppProvider, ChainId } from "@usedapp/core"
 import { Header } from "./components/Header"
-import {Container} from "@material-ui/core"
-import {Main} from "./components/Main"
+import { Container } from "@material-ui/core"
+import { Main } from "./components/Main"
 function App() {
   return (
     <DAppProvider config={{
-      supportedChains: [ChainId.Kovan, ChainId.Rinkeby]
+      supportedChains: [ChainId.Kovan]
     }}>
       <Header />
       <Container maxWidth="md">
         <div>Mi primera pagina web con TypeScript!</div>
-        <Main/>
+        <Main />
       </Container>
     </DAppProvider>
   );
