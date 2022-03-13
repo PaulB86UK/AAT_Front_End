222d8d8757515ac695e4ea708f4e09b94b8fa5d4
CrypptoPaul <84225765+CrypptoPaul@users.noreply.github.com>
2022-03-13 09:05:00 +0000

    13/03/2022

--- front_end/src/components/Main.tsx
+++ front_end/src/components/Main.tsx
@@ -1,9 +1,24 @@
+/* eslint-disable spaced-comment */
+/// <reference types="react-scripts" />
+
 import { useEthers } from "@usedapp/core"
 import { Button, makeStyles } from "@material-ui/core"
 import helperConfig from "../helper-config.json"
 import networkMapping from "../chain-info/deployments/map.json"
 import { constants } from "ethers"
 import brownieConfig from "../brownie-config.json"
+import aat_logo from "../img/aat.png"
+import eth_logo from "../img/eth.png"
+import dai_logo from "../img/dai.png"
+import { YourWallet } from "./yourWallet/YourWallet"
+
+export type Token = {
+    image: string,
+    addres: string,
+    name: string
+
+}
+
 export const Main = () => {
     //Show Tokens values from Wallets
     //Get the address of different tokens
@@ -12,5 +27,23 @@ export const Main = () => {
     const aatTokenAddress = chainId ? networkMapping[String(chainId)]["AAToken"][0] : constants.AddressZero
     const wethTokenAddress = chainId ? brownieConfig[networkName]["weth_token"] : constants.AddressZero
     const fauTokenAddress = chainId ? brownieConfig[networkName]["fau_token"] : constants.AddressZero
-    return (<div>Aloha, Imma Main!</div>)
+
+    const supportedTokens: Array<Token> = [
+        {
+            image: aat_logo,
+            addres: aatTokenAddress,
+            name: "AAT"
+        },
+        {
+            image: eth_logo,
+            addres: wethTokenAddress,
+            name: "ETH"
+        },
+        {
+            image: dai_logo,
+            addres: fauTokenAddress,
+            name: "DAI"
+        }
+    ]
+    return (<YourWallet supportedTokens={supportedTokens} />)
 }
